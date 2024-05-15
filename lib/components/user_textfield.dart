bd45adc27bada687edfa8220a2cbb13724ae30b7
nja99 <ngjianan@gmail.com>
2024-05-15 18:29:14 +0800

    Finalised CRUD Foundation / UI adjustments

--- lib/components/user_textfield.dart
+++ lib/components/user_textfield.dart
@@ -15,21 +15,21 @@ class UserTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
         controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
+          enabledBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white),
           ),
-          focusedBorder: OutlineInputBorder(
+          focusedBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.black),
           ),
           fillColor: Colors.grey.shade200,
           filled: true,
           hintText: hintText,
-          hintStyle: TextStyle(color: Colors.grey),
+          hintStyle: const TextStyle(color: Colors.grey),
         ),
       ),
     );
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -2,25 +2,39 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
 
-  // Retrieve Collection of Documents
+// Retrieve Collection of Documents
 
-    final CollectionReference documents =
-      FirebaseFirestore.instance.collection("documents");
+  final CollectionReference documents =
+    FirebaseFirestore.instance.collection("documents");
 
-  // CREATE
-    Future<void> addDocument(String document) {
-      return documents.add({
-        'document': document,
-        'time_stamp': Timestamp.now()
-      });
-    }
-
+// CREATE
+  Future<void> addDocument(String document) {
+    return documents.add({
+      'document': document,
+      'time_stamp': Timestamp.now()
+    });
+  }
 
   // READ
+  Stream<QuerySnapshot> readDocumentStream() {
+    final documentStream = 
+      documents.orderBy('time_stamp', descending: true).snapshots();
+    
+    return documentStream;
+  }
 
   // UPDATE
+  Future<void> updateDocument(String documentID, String newDocument) {
+    return documents.doc(documentID).update({
+      'document': newDocument,
+      'time_stamp': Timestamp.now()
+    });
+  }
 
   // DELETE
+  Future<void> deleteDocument(String documentID) {
+    return documents.doc(documentID).delete();
+  }
 
 
 }
