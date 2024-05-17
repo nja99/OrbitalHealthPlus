a9543924b65b34efd722de1283e5cd4fb0a3bbe0
saajidshaik02 <saajidshaik02@gmail.com>
2024-05-17 15:23:51 +0800

    o

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
   colorScheme: const ColorScheme.dark(
-    background: Color(0xFF352F44),
+    surface:Color(0xFF352F44),
     primary:Color(0xFF5C5470),
     secondary: Color(0xFFB9B4C7),
     tertiary: Color(0xFFFAF0E6),
